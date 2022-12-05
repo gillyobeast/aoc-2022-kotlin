00692aaf56301ec33d30e57ccf6553916838a7ba
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 14:21:29 +0000

    Day03 part1 - prep

---
+++ src/Day03.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("input_test")
+    val input = readInput("input")
+
+    // part 1
+    ::part1.forInput(testInput, returns = 1)
+    println("Part 1: ${part1(input)}")
+
+    // part 2
+    ::part2.forInput(testInput, returns = 1)
+    println("Part 2: ${part2(input)}")
+}
+
