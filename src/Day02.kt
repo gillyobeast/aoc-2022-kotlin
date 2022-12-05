ba2235edca532a05ddfc77415c8f974921043d33
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 14:00:11 +0000

    Day02 part1 - solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,7 +1,46 @@
+enum class Shape(val score: Int) {
+    ROCK(1),
+    PAPER(2),
+    SCISSORS(3), ;
+}
+
+
+fun String.toShape(): Shape {
+    return if ("X" == this || "A" == this) Shape.ROCK else if ("Y" == this || "B" == this) Shape.PAPER else Shape.SCISSORS
+}
+
+private fun youWin(you: Shape, opponent: Shape) =
+    ((you == Shape.ROCK && opponent == Shape.SCISSORS)
+            || (you == Shape.PAPER && opponent == Shape.ROCK)
+            || (you == Shape.SCISSORS && opponent == Shape.PAPER))
+
 fun main() {
 
+
+    fun outcomeScore(you: Shape, opponent: Shape): Int {
+        if (you == opponent) return 3
+        return if (youWin(you, opponent)) 6 else 0
+    }
+
+    fun toMap(input: List<String>): List<Pair<Shape, Shape>> =
+        input.map { val parts = it.split(' '); parts[0].toShape() to parts[1].toShape() }
+
     fun totalRpsScore(input: List<String>): Int {
-        return input.size
+        val pairs = toMap(input)
+        var score = 0
+        for ((opponent, you) in pairs) {
+            println("$opponent - $you")
+            val shapeScore = you.score
+            val outcomeScore = outcomeScore(you, opponent)
+            val thisScore = shapeScore + outcomeScore
+//            println("shapeScore = ${shapeScore}")
+//            println("outcomeScore = ${outcomeScore}")
+//            println("thisScore = ${thisScore}")
+//            println()
+            score += thisScore
+        }
+
+        return score
     }
 
     fun part2(input: List<String>): Int {
@@ -13,7 +52,7 @@ fun main() {
     val input = readInput("Day02")
 
     // part 1
-    ::totalRpsScore.forInput(testInput, returns = 16)
+    ::totalRpsScore.forInput(testInput, returns = 15)
     println("Part 1: ${totalRpsScore(input)}")
 
     // part 2
