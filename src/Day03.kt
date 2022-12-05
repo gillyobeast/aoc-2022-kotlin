5d3f858abfe7dbac2e97582a39e60b62e86eb6ef
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 14:48:25 +0000

    Day03 part1 - done

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,8 +1,36 @@
 fun main() {
 
+    val lowerPriorities =
+        ("abcdefghijklmnopqrstuvwxyz".toCharArray()).zip((1..26).toList()).toMap()
+    val upperPriorities =
+        ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()).zip((27..52).toList()).toMap()
+
+    fun splitBackpack(backpack: String): Pair<String, String> {
+        val n = backpack.length / 2
+        return backpack.substring(0, n) to backpack.substring(n, backpack.length)
+    }
+
+    fun findCommonItem(backPack: Pair<String, String>): Char {
+        for (i in backPack.first) {
+            if (backPack.second.contains(i.toString().toRegex())) {
+                return i
+            }
+        }
+        throw IllegalStateException("No common character found between halves of [$backPack]!")
+    }
+
+    fun toPriority(c: Char): Int {
+        return lowerPriorities[c] ?: upperPriorities[c]
+        ?: throw IllegalStateException("No priority found for [$c]!")
+    }
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map(::splitBackpack)
+            .map(::findCommonItem)
+            .map(::toPriority)
+            .sum()
+
     }
 
     fun part2(input: List<String>): Int {
@@ -14,7 +42,7 @@ fun main() {
     val input = readInput("input")
 
     // part 1
-    ::part1.forInput(testInput, returns = 1)
+    ::part1.forInput(testInput, returns = 157)
     println("Part 1: ${part1(input)}")
 
     // part 2
