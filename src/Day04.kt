9808ffe8f299f86e749effb64c03cf96d3c11c45
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 16:50:37 +0000

    Day04 part1 - attempt but apparently is wrong?

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,24 +1,37 @@
 fun main() {
 
 
-    fun toPair(it: String): Pair<String, String> {
-        val halves = it.split(","); return halves[0] to halves[1]
+    fun toPair(it: String, delimiter: String): Pair<String, String> {
+        val halves = it.split(delimiter)
+        return halves[0] to halves[1]
     }
 
     fun Pair<String, String>.toRange(): IntRange = first.toInt()..second.toInt()
 
-    fun String.toRange(): IntRange = toPair(this).toRange()
+    fun String.toRange(): IntRange = toPair(this, "-").toRange()
 
 
     fun parsePairs(input: List<String>) = input
-        .associate(::toPair)
+        .associate { toPair(it, ",") }
         .map { it.key.toRange() to it.value.toRange() }
         .toList()
 
-    fun part1(input: List<String>): Int {
-        val pairedUpRanges = parsePairs(input)
+    infix fun IntRange.fullyContains(other: IntRange) =
+        this.all { it in other }
 
-        return input.size
+    fun Pair<IntRange, IntRange>.overlaps(): Boolean {
+        val firstInSecond = (second.first <= first.first) and (first.last <= second.last)
+        val secondInFirst = (first.first <= second.first) and (second.last <= first.last)
+        print("Range [$this] - firstInSecond: $firstInSecond - secondInFirst: $secondInFirst - overall: ")
+        return (firstInSecond or secondInFirst).also(::println)
+    }
+
+
+    fun part1(input: List<String>): Int {
+        val (overlaps, _) = parsePairs(input)
+            .partition { it.overlaps() }
+
+        return overlaps.size
     }
 
     fun part2(input: List<String>): Int {
@@ -30,12 +43,13 @@ fun main() {
     val input = readInput("input")
 
     // part 1
-    ::part1.forInput(testInput, returns = 15)
+    ::part1.forInput(testInput, returns = 2)
     println("Part 1: ${part1(input)}")
 
     // part 2
-    ::part2.forInput(testInput, returns = 12)
+    ::part2.forInput(testInput, returns = 1)
     println("Part 2: ${part2(input)}")
 }
+
 
 
