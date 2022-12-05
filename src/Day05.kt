cb646b87da7adffd2c2974a555d0465611a7ab3f
Luke Armitage <gillyobeast@gmail.com>
2022-12-05 20:48:41 +0000

    Day05 part1 - works for test input but stacks are reordered for some reason

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,42 +1,68 @@
-import java.util.Stack
+import java.util.*
 
 fun main() {
 
-    fun List<String>.extractIndices() =
-        last().split("\\s+".toRegex()).filter (String::isNotBlank)
+    val whitespace = "\\s+".toRegex()
+    fun List<String>.extractIndices(): List<String> {
+        return last().split(whitespace).filter(String::isNotBlank)
+    }
 
     fun parse(input: List<String>): Pair<List<String>, List<String>> = input
         .filter(String::isNotEmpty)
         .partition { it.startsWith("move") }
 
-    fun buildStacks(indices: List<String>): List<Stack<Char>> {
-        val numberOfStacks = indices.last()
+    fun String.extractLabel(): Char {
+        return this[1]
+    }
+
+    fun buildStacks(stackData: List<String>): List<Stack<Char>> {
+        val numberOfStacks = stackData.extractIndices().last()
 
         val stacks = mutableListOf<Stack<Char>>()
         repeat(numberOfStacks.toInt()) {
             stacks.add(Stack())
         }
-        return stacks.toList()
+
+        stackData.dropLast(1).forEach {
+            it.split(whitespace).forEachIndexed { idx, item ->
+                if (item.isNotEmpty())
+                    stacks[idx].push(item.extractLabel())
+            }
+        }
+        return stacks.onEach { it.reverse() }.toList()
     }
 
-    fun part1(input: List<String>): Int {
+
+    fun String.applyTo(stacks: List<Stack<Char>>) {
+        val split = this.split(whitespace)
+        val count = split[1].toInt()
+        val source = stacks[split[3].toInt() - 1]
+        val target = stacks[split[5].toInt() - 1]
+
+        repeat(count){
+            target.push(source.pop())
+        }
+    }
+
+    fun List<String>.applyTo(stacks: List<Stack<Char>>) {
+        forEach { it.applyTo(stacks) }
+    }
+
+    fun part1(input: List<String>): String {
 
         val (instructions, stackData) = parse(input)
 
-        println("stackData: \n${stackData.joinToString("\n")}")
-        println("instructions: ${instructions}")
+        val stacks = buildStacks(stackData)
+        println("stacks = ${stacks}")
 
-        val indices = stackData.extractIndices()
-        println("indices = ${indices}")
-        val stacks = buildStacks(indices)
+        instructions.applyTo(stacks)
 
 
-
-        return input.size
+        return stacks.map { it.pop() }.joinToString("")
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): String {
+        return input.size.toString()
     }
 
     // test if implementation meets criteria from the description, like:
@@ -44,11 +70,14 @@ fun main() {
     val input = readInput("input")
 
     // part 1
-    ::part1.forInput(testInput, returns = 15)
+    ::part1.forInput(testInput, returns = "CMZ")
     println("Part 1: ${part1(input)}")
 
     // part 2
     ::part2.forInput(testInput, returns = 12)
     println("Part 2: ${part2(input)}")
 }
+
+
+
 
