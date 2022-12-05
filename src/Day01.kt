2ba853498bbbe12a58d65623199919d7ab210060
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 11:32:04 +0000

    prepare to attempt day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun maxTotalCalories(input: List<String>): Int {
         return input.size
     }
 
@@ -9,9 +9,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(maxTotalCalories(testInput) == 24000) { "expected 24000 but was ${maxTotalCalories(testInput)}"}
 
     val input = readInput("Day01")
-    println(part1(input))
+    println(maxTotalCalories(input))
     println(part2(input))
 }
