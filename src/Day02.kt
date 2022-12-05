ff49a44bdda9787e58ffb74014a5821819154d9b
Luke Armitage <luke.armitage@sky.uk>
2022-12-05 14:12:51 +0000

    Day02 part2 - solved re-did the mapping and the calculation is the same

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,6 +4,33 @@ enum class Shape(val score: Int) {
     SCISSORS(3), ;
 }
 
+// X == lose
+// Y == draw
+// Z == win!
+
+fun String.toTheirShape(): Shape {
+    return when (this) {
+        "A" -> Shape.ROCK
+        "B" -> Shape.PAPER
+        else -> Shape.SCISSORS
+    }
+}
+
+fun String.toYourShape(theirs: Shape): Shape {
+    return when(this) {
+        "X" -> when(theirs){
+            Shape.ROCK -> Shape.SCISSORS
+            Shape.PAPER -> Shape.ROCK
+            Shape.SCISSORS -> Shape.PAPER
+        }
+        "Y" -> theirs
+        else -> when(theirs){
+            Shape.ROCK -> Shape.PAPER
+            Shape.PAPER -> Shape.SCISSORS
+            Shape.SCISSORS -> Shape.ROCK
+        }
+    }
+}
 
 fun String.toShape(): Shape {
     return if ("X" == this || "A" == this) Shape.ROCK else if ("Y" == this || "B" == this) Shape.PAPER else Shape.SCISSORS
@@ -25,11 +52,15 @@ fun main() {
     fun toMap(input: List<String>): List<Pair<Shape, Shape>> =
         input.map { val parts = it.split(' '); parts[0].toShape() to parts[1].toShape() }
 
-    fun totalRpsScore(input: List<String>): Int {
-        val pairs = toMap(input)
+    fun toMapPart2(input: List<String>): List<Pair<Shape, Shape>> =
+        input.map { val parts = it.split(' ')
+            val theirShape = parts[0].toTheirShape()
+            theirShape to parts[1].toYourShape(theirShape) }
+
+    fun calculateScore(pairs: List<Pair<Shape, Shape>>): Int {
         var score = 0
         for ((opponent, you) in pairs) {
-            println("$opponent - $you")
+//            println("$opponent - $you")
             val shapeScore = you.score
             val outcomeScore = outcomeScore(you, opponent)
             val thisScore = shapeScore + outcomeScore
@@ -43,8 +74,13 @@ fun main() {
         return score
     }
 
+    fun totalRpsScore(input: List<String>): Int {
+        val pairs = toMap(input)
+        return calculateScore(pairs)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculateScore(toMapPart2(input))
     }
 
     // test if implementation meets criteria from the description, like:
@@ -56,7 +92,7 @@ fun main() {
     println("Part 1: ${totalRpsScore(input)}")
 
     // part 2
-    ::part2.forInput(testInput, returns = 1)
+    ::part2.forInput(testInput, returns = 12)
     println("Part 2: ${part2(input)}")
 }
 
