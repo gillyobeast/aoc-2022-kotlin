f01e4ac2d15bdecaa80e28065cc17bd632a47198
Luke Armitage <luke.armitage@sky.uk>
2022-12-07 08:37:21 +0000

    Day07 prep

---
+++ src/day07/Day07.kt
@@ -0,0 +1,28 @@
+package day07
+
+import appliedTo
+import readInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("input_test")
+    val input = readInput("input")
+
+    // part 1
+    ::part1.appliedTo(testInput, returns = 95437)
+    println("Part 1: ${part1(input)}")
+
+    // part 2
+    ::part2.appliedTo(testInput, returns = -1)
+    println("Part 2: ${part2(input)}")
+}
+
