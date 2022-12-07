dec9d60fdf78987d57725ca11a14121695842b52
Luke Armitage <luke.armitage@sky.uk>
2022-12-07 08:53:59 +0000

    Day07 - outline strategy

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -3,6 +3,21 @@ package day07
 import appliedTo
 import readInput
 
+
+/// strategy:
+//  add `/` to tree
+//  dirname = "/"
+//  split input by lines starting $
+//  for each of those:
+//      if `cd ..`, set dirname to 'parent'
+//      if `cd dirname` set `currentDir` to `dirname`
+//      if `ls`, iterate over remaining lines:
+//          if starts with `\d+`, add child with that size and name that follows
+//          else if starts with `dir dirname`, add child with name `dirname`
+
+//  then traverse the tree, grabbing all `dir`s with size < limit and adding to list:
+//  list.sumOf { it.size }
+
 fun part1(input: List<String>): Int {
     return input.size
 }
