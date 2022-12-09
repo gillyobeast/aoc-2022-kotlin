024f0383e008732025a98f09f111e8491337887b
Armitage, Luke <luke.armitage@sky.uk>
2022-12-09 16:05:06 +0000

    Day 9 - part 2 - after much trouble, sort my diagonal movement

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -2,10 +2,9 @@ package day09
 
 import utils.appliedTo
 import utils.readInput
-import java.util.List.copyOf
 import kotlin.math.abs
 
-enum class Move(val regex: Regex, val applyTo: (Pair<Int, Int>) -> Pair<Int, Int>) {
+enum class Move(val regex: Regex, val applyTo: (Point) -> Point) {
     UP("U \\d+".toRegex(), ::incrementY),
     DOWN("D \\d+".toRegex(), ::decrementY),
     LEFT("L \\d+".toRegex(), ::decrementX),
@@ -20,11 +19,13 @@ enum class Move(val regex: Regex, val applyTo: (Pair<Int, Int>) -> Pair<Int, Int
     }
 }
 
+typealias Point = Pair<Int, Int>
+
 fun part1(input: List<String>): Int {
 
     val moves = input.map { Move.of(it) }
 
-    println(moves)
+//    println(moves)
 
     var headPosition = 0 to 0 // x to y
     var tailPosition = 0 to 0 // x to y
@@ -32,20 +33,17 @@ fun part1(input: List<String>): Int {
 
     moves.forEach { (move, step): Pair<Move, Int> ->
         repeat(step) { _ ->
-            val previousHead = headPosition
             headPosition = move.applyTo(headPosition)
-            print("head = ${headPosition} ")
+//            print("head = $headPosition ")
 
-            tailPosition =
-                if (tailPosition.isWithin1Of(headPosition)) tailPosition
-                else previousHead
+            tailPosition = tailPosition.moveDiagonallyTowards(headPosition)
 
-            println("tail = ${tailPosition}")
+//            println("tail = $tailPosition")
 
             tailVisitedPositions.add(tailPosition)
         }
     }
-    println(tailVisitedPositions)
+//    println(tailVisitedPositions)
 
     return tailVisitedPositions.size
 }
@@ -55,56 +53,74 @@ fun part2(input: List<String>): Int {
 
     val moves = input.map { Move.of(it) }
 
-    println(moves)
+//    println(moves)
 
 // head, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    val knots = mutableListOf<Pair<Int, Int>>()
+    val knots = mutableListOf<Point>()
 
     repeat(10) { knots.add(0 to 0) }
     val tailVisitedPositions = mutableSetOf(knots[9])
 
     moves.forEach { (move, step): Pair<Move, Int> ->
         repeat(step) { _ ->
-            val previous = copyOf(knots)
 
             knots[0] = move.applyTo(knots[0])
 //            print("head = ${knots[0]}, ")
             for (i in 1..9) {
-                fun Pair<Int, Int>.moveToward(headPosition: Pair<Int, Int>) =
-                    if (isWithin1Of(headPosition)) this else previous[i - 1]
 
-                knots[i] = knots[i].moveToward(knots[i - 1])
+                knots[i] = knots[i].moveDiagonallyTowards(knots[i - 1])
+//                print("; $i = ${knots[i]}, ")
             }
+//            println()
 
-            tailVisitedPositions.add(knots[9]/* .also { println("tail = $it") }*/)
+            tailVisitedPositions.add(knots[9])
         }
-        for (i in 0..9)
-            print("$i = ${knots[i]}, ")
-        println()
+//        for (i in 0..9)
+//            print("$i = ${knots[i]}, ")
+//        println()
     }
-    println(tailVisitedPositions)
+//    println(tailVisitedPositions)
 
     return tailVisitedPositions.size
 }
 
+private fun Point.moveDiagonallyTowards(other: Point): Point {
+    this.log("start ")
+    other.log(", towards ")
+    return if (this.isWithin1Of(other)) this
+    else {
+        val x = first.moveToward(other.first)
+        val y = second.moveToward(other.second)
+        (x to y).log(", moving to ")
+    }
+}
 
-private fun Pair<Int, Int>.isWithin1Of(headPosition: Pair<Int, Int>) =
-    abs(first - headPosition.first) <= 1 && abs(second - headPosition.second) <= 1
+private fun Int.moveToward(first1: Int) =
+    if (this == first1) this else if (this < first1) this + 1 else this - 1
+
+
+private fun Point.isWithin1Of(headPosition: Point): Boolean {
+    return abs(first - headPosition.first).log(", x ") <= 1 && abs(second - headPosition.second).log(
+        ", y "
+    ) <= 1
+}
+
+private fun <T> T.log(s: String = ""): T = this//.also { print(s + it) }
 
 private fun incrementX(
-    headPosition: Pair<Int, Int>
+    headPosition: Point
 ) = headPosition.first + 1 to headPosition.second
 
 private fun decrementX(
-    headPosition: Pair<Int, Int>
+    headPosition: Point
 ) = headPosition.first - 1 to headPosition.second
 
 private fun decrementY(
-    headPosition: Pair<Int, Int>
+    headPosition: Point
 ) = headPosition.first to headPosition.second - 1
 
 private fun incrementY(
-    headPosition: Pair<Int, Int>,
+    headPosition: Point,
 ) = headPosition.first to headPosition.second + 1
 
 fun main() {
@@ -113,8 +129,8 @@ fun main() {
     val input = readInput("input")
 
     // part 1
-//    ::part1.appliedTo(testInput, returns = 88)
-//    println("Part 1: ${part1(input)}")
+    ::part1.appliedTo(testInput, returns = 88)
+    println("Part 1: ${part1(input)}")
 
     // part 2
     ::part2.appliedTo(testInput, returns = 36)
