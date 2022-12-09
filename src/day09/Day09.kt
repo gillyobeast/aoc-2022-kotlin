d67e5e565fa485545df14ea745bf775d92707863
Armitage, Luke <luke.armitage@sky.uk>
2022-12-09 15:10:50 +0000

    Day 9 - part 2 naive attempt

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -2,10 +2,10 @@ package day09
 
 import utils.appliedTo
 import utils.readInput
+import java.util.List.copyOf
 import kotlin.math.abs
-import kotlin.reflect.KFunction2
 
-enum class Move(val regex: Regex, val apply: KFunction2<Pair<Int, Int>, Int, Pair<Int, Int>>) {
+enum class Move(val regex: Regex, val applyTo: (Pair<Int, Int>) -> Pair<Int, Int>) {
     UP("U \\d+".toRegex(), ::incrementY),
     DOWN("D \\d+".toRegex(), ::decrementY),
     LEFT("L \\d+".toRegex(), ::decrementX),
@@ -31,9 +31,9 @@ fun part1(input: List<String>): Int {
     val tailVisitedPositions = mutableSetOf(tailPosition)
 
     moves.forEach { (move, step): Pair<Move, Int> ->
-        repeat(step) {
+        repeat(step) { _ ->
             val previousHead = headPosition
-            headPosition = move.apply(headPosition, 1)
+            headPosition = move.applyTo(headPosition)
             print("head = ${headPosition} ")
 
             tailPosition =
@@ -50,34 +50,62 @@ fun part1(input: List<String>): Int {
     return tailVisitedPositions.size
 }
 
+
+fun part2(input: List<String>): Int {
+
+    val moves = input.map { Move.of(it) }
+
+    println(moves)
+
+// head, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    val knots = mutableListOf<Pair<Int, Int>>()
+
+    repeat(10) { knots.add(0 to 0) }
+    val tailVisitedPositions = mutableSetOf(knots[9])
+
+    moves.forEach { (move, step): Pair<Move, Int> ->
+        repeat(step) { _ ->
+            val previous = copyOf(knots)
+
+            knots[0] = move.applyTo(knots[0])
+//            print("head = ${knots[0]}, ")
+            for (i in 1..9) {
+                fun Pair<Int, Int>.moveToward(headPosition: Pair<Int, Int>) =
+                    if (isWithin1Of(headPosition)) this else previous[i - 1]
+
+                knots[i] = knots[i].moveToward(knots[i - 1])
+            }
+
+            tailVisitedPositions.add(knots[9]/* .also { println("tail = $it") }*/)
+        }
+        for (i in 0..9)
+            print("$i = ${knots[i]}, ")
+        println()
+    }
+    println(tailVisitedPositions)
+
+    return tailVisitedPositions.size
+}
+
+
 private fun Pair<Int, Int>.isWithin1Of(headPosition: Pair<Int, Int>) =
     abs(first - headPosition.first) <= 1 && abs(second - headPosition.second) <= 1
 
-
-
 private fun incrementX(
-    headPosition: Pair<Int, Int>,
-    step: Int
-) = headPosition.first + step to headPosition.second
+    headPosition: Pair<Int, Int>
+) = headPosition.first + 1 to headPosition.second
 
 private fun decrementX(
-    headPosition: Pair<Int, Int>,
-    step: Int
-) = headPosition.first - step to headPosition.second
+    headPosition: Pair<Int, Int>
+) = headPosition.first - 1 to headPosition.second
 
 private fun decrementY(
-    headPosition: Pair<Int, Int>,
-    step: Int
-) = headPosition.first to headPosition.second - step
+    headPosition: Pair<Int, Int>
+) = headPosition.first to headPosition.second - 1
 
 private fun incrementY(
     headPosition: Pair<Int, Int>,
-    step: Int
-) = headPosition.first to headPosition.second + step
-
-fun part2(input: List<String>): Int {
-    return input.size
-}
+) = headPosition.first to headPosition.second + 1
 
 fun main() {
 
@@ -85,11 +113,11 @@ fun main() {
     val input = readInput("input")
 
     // part 1
-    ::part1.appliedTo(testInput, returns = 13)
-    println("Part 1: ${part1(input)}")
+//    ::part1.appliedTo(testInput, returns = 88)
+//    println("Part 1: ${part1(input)}")
 
     // part 2
-    ::part2.appliedTo(testInput, returns = -1)
+    ::part2.appliedTo(testInput, returns = 36)
     println("Part 2: ${part2(input)}")
 }
 
