e92ec388ef760b1f2458e0a41de311fd823a994b
Armitage, Luke <luke.armitage@sky.uk>
2022-12-09 11:20:24 +0000

    Day 9 - turn input into a list of moves

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -3,7 +3,27 @@ package day09
 import utils.appliedTo
 import utils.readInput
 
+enum class Move(val regex: Regex) {
+    DOWN("D \\d+".toRegex()),
+    UP("U \\d+".toRegex()),
+    LEFT("L \\d+".toRegex()),
+    RIGHT("R \\d+".toRegex());
+
+    companion object {
+        private val digits = "(\\d+)".toRegex()
+        fun of(string: String): Pair<Move, Int> {
+            val direction = Move.values().first { it.regex.matches(string) }
+            return direction to digits.find(string)!!.value.toInt()
+        }
+    }
+}
+
 fun part1(input: List<String>): Int {
+
+    val moves = input.map { Move.of(it) }
+
+    println(moves)
+
     return input.size
 }
 
